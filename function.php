d4dac7da75eef6483eb675689f986e1709a2ba75
shakibcs <shakibcste92@gmail.com>
2020-04-27 03:28:33 +0600

    calculation class and method

---
+++ function.php
@@ -0,0 +1,23 @@
+
+	<?php 
+
+class Calculation{
+
+	public function add($a,$b){
+		echo "addition is :". ($a+$b). "<br>";
+	}
+
+	public function sub($a,$b){
+		echo "Substraction is :". ($a-$b). "<br>";
+	}
+
+	public function mul($a,$b){
+		echo "Multiplication is :". $a*$b."<br>";
+	}
+
+	public function div($a,$b){
+		echo "Division is :". $a/$b;
+	}
+
+
+}
